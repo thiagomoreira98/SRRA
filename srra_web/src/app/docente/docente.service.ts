cfd725754838ec56f2e5bf193d3b18923f5c8395
juninmd <jr_acn@yahoo.com.br>
2017-10-15 13:52:04 -0200

    Refatorando a url de services Com template string

--- srra_web/src/app/docente/docente.service.ts
+++ srra_web/src/app/docente/docente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { environment } from './../../environments/environment.prod';
+import { environment } from './../../environments/environment';
 
 @Injectable()
 export class DocenteService {
@@ -9,22 +9,22 @@ export class DocenteService {
   constructor(private http: HttpClient) { }
 
   selecionar() {
-    return this.http.get(environment.urlApi + '/api/docente');
+    return this.http.get(`${environment.urlApi}/api/docente`);
   }
 
   buscar(id: any): any {
-    return this.http.get(environment.urlApi + '/api/docente/' + id);
+    return this.http.get(`${environment.urlApi}/api/docente/${id}`);
   }
 
   inserir(docente: any) {
-    return this.http.post(environment.urlApi + '/api/docente', docente).toPromise();
+    return this.http.post(`${environment.urlApi}/api/docente`, docente).toPromise();
   }
 
   alterar(id: any, docente: any) {
-    return this.http.put(environment.urlApi + '/api/docente/' + id, docente).toPromise();
+    return this.http.put(`${environment.urlApi}/api/docente/${id}`, docente).toPromise();
   }
 
   deletar(id: any) {
-    return this.http.delete(environment.urlApi + '/api/docente/' + id).toPromise();
+    return this.http.delete(`${environment.urlApi}/api/docente/${id}`).toPromise();
   }
 }
--- srra_web/src/app/laboratorio/laboratorio.service.ts
+++ srra_web/src/app/laboratorio/laboratorio.service.ts
@@ -9,22 +9,22 @@ export class LaboratorioService {
   constructor(private http: HttpClient) { }
 
   selecionar(): any {
-    return this.http.get(environment.urlApi + '/api/laboratorio');
+    return this.http.get(`${environment.urlApi}/api/laboratorio`);
   }
 
   buscar(id: any): any {
-    return this.http.get(environment.urlApi + '/api/laboratorio/' + id);
+    return this.http.get(`${environment.urlApi}/api/laboratorio/${id}`);
   }
 
   inserir(laboratorio: any) {
-    return this.http.post(environment.urlApi + '/api/laboratorio', laboratorio).toPromise();
+    return this.http.post(`${environment.urlApi}/api/laboratorio`, laboratorio).toPromise();
   }
 
   alterar(id: any, laboratorio: any) {
-    return this.http.put(environment.urlApi + '/api/laboratorio/' + id, laboratorio).toPromise();
+    return this.http.put(`${environment.urlApi}/api/laboratorio/${id}`, laboratorio).toPromise();
   }
 
   deletar(id: any) {
-    return this.http.delete(environment.urlApi + '/api/laboratorio/' + id).toPromise();
+    return this.http.delete(`${environment.urlApi}/api/laboratorio/${id}`).toPromise();
   }
 }
