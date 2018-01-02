f9a55760f4fd2708197fabe703894738ef14ca43
Thiago Moreira <thiago.gontijo@smn.com.br>
2018-01-02 15:45:44 -0200

    organizando rotas

--- srra_web/src/app/app-routing.module.ts
+++ srra_web/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+
 import { RecursoFormComponent } from './recurso/recurso-form/recurso-form.component';
 import { RecursoGridComponent } from './recurso/recurso-grid/recurso-grid.component';
 
@@ -18,18 +19,28 @@ import { OcorrenciaGridComponent } from './ocorrencia/ocorrencia-grid/ocorrencia
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+
+  //Recursos
   { path: 'recursos',  component: RecursoGridComponent },
-  { path: 'recursos/cadastrar', component: RecursoFormComponent },
-  { path: 'recursos/info/:id', component: RecursoFormComponent},
-  { path: 'docentes', component: DocenteGridComponent },
-  { path: 'docentes/cadastrar', component: DocenteFormComponent },
-  { path: 'docentes/info/:id', component: DocenteFormComponent },
+  { path: 'recursos/novo', component: RecursoFormComponent },
+  { path: 'recursos/info/:id', component: RecursoFormComponent },
+  
+  //Laboratorios
   { path: 'laboratorios', component: LaboratorioGridComponent },
-  { path: 'laboratorios/cadastrar', component: LaboratorioFormComponent },
+  { path: 'laboratorios/novo', component: LaboratorioFormComponent },
   { path: 'laboratorios/info/:id', component: LaboratorioFormComponent },
+
+  //Docentes
+  { path: 'docentes', component: DocenteGridComponent },
+  { path: 'docentes/novo', component: DocenteFormComponent },
+  { path: 'docentes/info/:id', component: DocenteFormComponent },
+
+  //Ocorrencias
   { path: 'ocorrencias', component: OcorrenciaGridComponent },
-  { path: 'ocorrencias/cadastrar', component: OcorrenciaFormComponent },
+  { path: 'ocorrencias/nova', component: OcorrenciaFormComponent },
   { path: 'ocorrencias/info/:id', component: OcorrenciaFormComponent },
+
+  //Reservas
 ];
 
 @NgModule({
